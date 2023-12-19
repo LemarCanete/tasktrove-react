aa73bdeb6450c6d81cedf135353f57ab5940512a
Lemar <lemarcanete4@gmail.com>
2023-12-20 00:18:53 +0800

    1st commit

--- app/Register/page.jsx
+++ app/Register/page.jsx
@@ -1,4 +1,3 @@
-'useClient'
 import React from 'react'
 import Link from 'next/link'
 
--- app/Reports/page.jsx
+++ app/Reports/page.jsx
@@ -1,4 +1,3 @@
-'use client'
 import React from 'react'
 import { SideNav } from '../components'
 import Reports from './Reports'
